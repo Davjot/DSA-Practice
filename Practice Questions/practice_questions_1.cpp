e46eb8a612b00ec3317d67f38fe5a6d249b8f34a
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-01 15:06:48 +0530

    Add files via upload

---
+++ Practice Questions/practice_questions_1.cpp
@@ -0,0 +1,71 @@
+// Q1
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 9;
+
+//     if (a == 9)
+//     {
+//         cout << "NINEY" << endl;
+//     }
+//     if (a > 0)
+//     {
+//         cout << "POSITIVE" << endl; 
+//     }
+//     else{
+//         cout << "NEGATIVE" << endl;
+//     }
+    
+//     return 0;
+// }
+
+// OUTPUT : 
+// NINEY
+// POSITIVE
+
+// Q2
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 2;
+//     int b = a + 1;
+
+//     if ((a = 3) == b)
+//     {
+//         cout << a << endl;
+//     }
+//     else{
+//         cout << a + 1 << endl;
+//     }
+    
+//     return 0;
+// }
+
+// OUTPUT:
+// 3
+
+// Q3
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 24;
+
+    if (a > 20)
+    {
+        cout << "DSA" << endl;
+    }
+    else if (a == 24)
+    {
+        cout << "DSA DSA" << endl;
+    }
+    else{
+        cout << "DSA DSA DSA" << endl;
+    }
+
+    return 0;
+}
+// OUTPUT:
+// DSA
