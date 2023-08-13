920e81c8b328cc99b694c6eb05f7361214ae0e57
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-14 03:17:48 +0530

    Add files via upload

---
+++ day 6/check_palindrome_of_char_array.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+// this function is used to just check whether the string is palindrome or not
+// bool checkPalindrome(char a[], int n){
+//     int s = 0;
+//     int e = n - 1;
+
+//     while(s <= e){
+//         if(a[s] != a[e]){
+//             return 0;
+//         }
+//         else{
+//             s++;
+//             e--;
+//         }
+//     }
+//     return 1;
+// }
+
+// this function is used to convert all the upper case letters to lower case if present inside a string or char array
+char toLowercase(char ch){
+    if(ch >= 'a' && ch <= 'z'){
+        return ch;
+    }
+    else{
+        char temp = ch - 'A' + 'a';
+        return temp;
+    }
+}
+
+bool checkPalindrome(char a[], int n){
+    int s = 0;
+    int e = n - 1;
+
+    while(s <= e){
+        if(toLowercase(a[s]) != toLowercase(a[e])){
+            return 0;
+        }
+        else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+
+int getlength(char name[]){
+    int count = 0;
+    for(int i = 0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    char name[20];
+
+    cout << "Enter your name : " << endl;
+    cin >> name;
+
+    cout << "Your name is : " << name << endl;
+
+    int len = getlength(name);
+
+    cout << "The string is Palindrome or not : " << checkPalindrome(name, len) << endl;
+
+    return 0;
+}
