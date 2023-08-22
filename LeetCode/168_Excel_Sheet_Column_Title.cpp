243bc37021fb3a32d5eef7852d32c06125810d07
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-22 17:35:48 +0530

    Create 168_Excel_Sheet_Column_Title.cpp

---
+++ LeetCode/168_Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,26 @@
+// There are two ways to solve this question
+// First : Iteratively
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        while (columnNumber) {
+            columnNumber--;
+            char c = 'A' + columnNumber % 26;
+            result = c + result;
+            columnNumber /= 26;
+        }
+        return result;
+    }
+};
+
+// Second : Recursively
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        if (columnNumber == 0) return "";
+        columnNumber--;
+        char c = 'A' + columnNumber % 26;
+        return convertToTitle(columnNumber / 26) + c;
+    }
+};
