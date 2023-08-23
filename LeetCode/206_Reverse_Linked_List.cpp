b5274ad3bd65e3f5f27c200898294d52059c8056
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-23 20:33:18 +0530

    Create 206_Reverse_Linked_List.cpp

---
+++ LeetCode/206_Reverse_Linked_List.cpp
@@ -0,0 +1,33 @@
+// There are two methods for reversing a linked list : Iteratively and Recursively 
+// Iteratively
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+            
+        }
+        return prev;
+    }
+};
+
+// Recursively
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* prev = NULL;
+        ListNode* h2 = reverseList(head->next);
+        head->next->next = head;
+        head->next=prev;
+        return h2;
+    }
+
+};
