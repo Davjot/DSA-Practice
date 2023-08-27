f362911cb69db90ab32a4850e06265e713e1c91e
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-28 01:47:14 +0530

    Create 416_Partition_Equal_Subset_Sum.cpp

---
+++ LeetCode/416_Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int target = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            target = target + nums[i];
+        }
+        if(target % 2 != 0){
+            return false;
+        }
+        target = target / 2;
+
+        unordered_set <int> dp;
+        dp.insert(0);
+
+        for(int i = 0; i < nums.size(); i++){
+            unordered_set <int> dpnext;
+            for(auto it = dp.begin(); it != dp.end(); it++){
+                if(*it + nums[i] == target){
+                    return true;
+                }
+                dpnext.insert(*it + nums[i]);
+                dpnext.insert(*it);
+            }
+            dp = dpnext;
+        }
+        return false;
+    }
+};
