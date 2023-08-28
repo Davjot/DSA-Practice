5dc51f55d308356c1cc46526c93ea9bf9c933915
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-28 14:46:12 +0530

    Create 169_Majority_Element.cpp

---
+++ LeetCode/169_Majority_Element.cpp
@@ -0,0 +1,51 @@
+// This approach is Sorting
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return nums[n / 2]; 
+    }
+};
+
+// This appraoch is Hash map approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> m;
+        
+        for(int i = 0; i < n; i++){
+            m[nums[i]]++;
+        }
+        n = n/2;
+        for(auto x: m){
+            if(x.second > n){
+                return x.first;
+            }
+        }
+        return 0;
+    }
+};
+
+// This approach is called Moore Voting 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }  
+        return candidate;
+    }
+};
