9be1efab02fa1e29135fc386ae6fa77590725c27
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-08-28 23:14:12 +0530

    Create 342_Power_of_Four.cpp

---
+++ LeetCode/342_Power_of_Four.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    float logbase(int a, int b){
+        return log(a) / log(b);
+    }
+
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 0){
+            return false;
+        }
+        float x = logbase(n, 4);
+        if(floor(x) == x){
+            return true;
+        }
+        return false;
+    }
+};
