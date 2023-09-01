c78f7e2815621fc4584356f29887d6550bce5826
Davjot Singh <71970250+Davjot@users.noreply.github.com>
2023-09-02 01:38:40 +0530

    Create 121_Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ LeetCode/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = prices[0];
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            int diff = prices[i] - minimum;
+            profit = max(profit, diff);
+            minimum = min(minimum, prices[i]);
+        }
+        return profit;
+    }
+};
